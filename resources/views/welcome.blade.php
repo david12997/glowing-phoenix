83ec1308a29149c75d06917f9d2e91c22014ae4a
david12997 <david12997@outlook.com>
2022-09-30 20:57:21 -0500

    primera maqueta

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Glowing Phoenix</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="/css/index.css">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    </head>
+    <body>
+
+        <video muted webkit-playsinline loop autoplay preload="none" poster>
+            <source src="/assets/video-bg.mp4" />
+        </video>
+
+        <div class="bg-black">
+
+
+            <div class="animation">
+                <img src="/assets/hand.webp" alt="" />
+            </div>
+            <h1 class="logo-mobile" >
+                <img src="/assets/logo-celu.webp" alt="glowing phoenix logo banda de metal melodico colombiana " srcset="">
+            </h1>
+
+            <h1 class="logo-desktop" >
+                <img src="/assets/logo-name.webp" alt="glowing phoenix logo banda de metal melodico colombiana " srcset="">
+            </h1>
+
+            <div class="info">
+                <p>
+                    <b>
+                        <i class="fa-solid fa-bomb"></i> Próximamente lanzamiento de nuestra primera
+                        canción oficial y presentación en vivo
+                        <br><br>
+                        <i class="fa-solid fa-music-note"></i>  <i class="fa-solid fa-music"></i> Siguenos Para disfrutar el metal al estilo y ritmo de Glowing Phoenix
+                    </b>
+                </p>
+
+            </div>
+
+
+            <div class="social-media">
+                <span>
+                    <i class="fa-brands fa-facebook"></i>
+                </span>
+                <span>
+                    <i class="fa-brands fa-instagram"></i>
+                </span>
+                <span>
+                    <i class="fa-brands fa-youtube"></i>
+                </span>
+            </div>
+
+
+            <div class="text">
+                <small><b>Politicas de privacidad -</b>  <b>- Terminos y condiciones</b></small>
+                <small><b> <i class="fa-solid fa-copyright"></i> 2022 glowingphoenix.co todos los derechos reservados</b></small>
+            </div>
+
+
+        </div>
+
+    </body>
+
+</html>
